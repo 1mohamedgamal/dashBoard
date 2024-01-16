0d78e14617cbb7e4b2df41d9db70c852625ba6fb
1mohamedgamal <mohamedgamal.sw@gmail.com>
2024-01-16 21:32:09 +0200

    setup

---
+++ src/app/shared/matriel/matriel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-matriel',
+  templateUrl: './matriel.component.html',
+  styleUrls: ['./matriel.component.css']
+})
+export class MatrielComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
